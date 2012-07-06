3069fca4ee180ce5e8fa0f476d7b6f8783d5647c
Owen Wang <wangfowen@gmail.com>
2012-07-06 00:31:02 -0700

    fixed manifest / added onclicks. bare basics is done and working

--- gen/banana/slayers/R.java
+++ gen/banana/slayers/R.java
@@ -26,5 +26,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int load_game=0x7f040001;
+        public static final int new_game=0x7f040002;
     }
 }
